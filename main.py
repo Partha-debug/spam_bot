d97cd238666ad8701006f97d42d88446eb72cce9
Partha Sarathi Dash <65017143+Partha-debug@users.noreply.github.com>
2020-10-04 17:00:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pyautogui, time
-words = set(open('message.txt').read().split())
-print('within 5 seconds open Whatsapp web or any messaging ap and click the curser on the text field where messages were to be typed.')
-print("The spamming will start in three seconds...")
-time.sleep(5)
-print("Spamming started.")
-while True:
-    for word in words:
-        pyautogui.typewrite(word + " ")
-        pyautogui.press('enter')
+
+def spam():
+    words = set(open('message.txt').read().split())
+    while True:
+        for word in words:
+            pyautogui.typewrite(word + " ")
+            pyautogui.press('enter')
+
+
+if __name__ == "__main__":
+    print('within 5 seconds open Whatsapp web or any other messaging app and click the curser on the text field where messages were to be typed.')
+    print("The spamming will start in three seconds...")
+    time.sleep(5)
+    print("Spamming started.")
+    spam() 
+
